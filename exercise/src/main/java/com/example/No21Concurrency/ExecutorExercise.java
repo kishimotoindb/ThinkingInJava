b759520d91c7a36eb6bd7413c90aea80e295bf59
cuihaichen@macpro <519376470@qq.com>
2018-02-26 21:54:08 +0800

    Callable

--- exercise/src/main/java/com/example/No21Concurrency/ExecutorExercise.java
+++ exercise/src/main/java/com/example/No21Concurrency/ExecutorExercise.java
@@ -1,7 +1,10 @@
 package com.example.No21Concurrency;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 /**
  * Created by BigFaceBear on 2018.02.26
@@ -10,10 +13,20 @@ import java.util.concurrent.Executors;
 public class ExecutorExercise {
     public static void main(String[] args) {
         ExecutorService es = Executors.newCachedThreadPool();
-        for (int i = 0; i < 10; i++) {
-            es.execute(new MyRunnable());
-        }
+
+//        for (int i = 0; i < 10; i++) {
+//            es.execute(new MyRunnable());
+//        }
+
+        Future<String> future = es.submit(new MyCallable());
         es.shutdown();
+        try {
+            System.out.println(future.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
     }
 
     static class MyRunnable implements Runnable {
@@ -27,9 +40,17 @@ public class ExecutorExercise {
         @Override
         public void run() {
             for (int i = 0; i < 10; i++) {
-                System.out.println(mIndex + "(" + i + ")");
+                System.out.print(mIndex + "(" + i + ")    ");
             }
         }
     }
+
+    static class MyCallable implements Callable<String> {
+        @Override
+        public String call() throws Exception {
+            return "MyCallable";
+        }
+    }
+
 
 }
